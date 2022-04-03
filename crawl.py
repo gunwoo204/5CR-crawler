b772e029b9e2acd7d6303a0145648841d98617e8
Geonwoo Lee <gunwoo204@unist.ac.kr>
2022-04-03 22:09:55 +0900

    조선일보 크롤링

--- crawl.py
+++ crawl.py
@@ -4,10 +4,31 @@ import json
 import requests
 from bs4 import BeautifulSoup
 
+HEADERS = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.83 Safari/537.36',
+}
+
 
 # 보수
 def chosun(url: str) -> str:    # 조선일보
-    pass
+    resp = requests.get(url, headers=HEADERS)
+    soup = BeautifulSoup(resp.text, 'lxml')
+
+    page_text = soup.find('script', id='fusion-metadata').text
+
+    semicolon_indexs = result = [_.start() for _ in re.finditer(';', page_text)]  # 세미콜론 위치(index) 모두 가져옴
+    global_content = page_text[semicolon_indexs[3]:semicolon_indexs[4]].replace(';Fusion.globalContent=', '')  # semicolon 4~5번째 사이의 내용을 가져와서 변수명 삭제
+    content = json.loads(global_content)  # 그럼 json으로 만들 수 있음
+
+    article_contents = content['content_elements']  # 기사 본문
+
+    pure_articles = []
+    for block in article_contents:
+        if 'content' not in block:  # 이미지 block은 content가 없음
+            continue
+        pure_articles.append(block['content'])
+
+    print('\n'.join(pure_articles))
 
 
 def joongang(url: str) -> str:  # 중앙일보
